7595a6b6cb010fe0d3cbe6581ed191057d592eac
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-03-16 18:05:37 +0200

    Added some validates

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 class Course < ActiveRecord::Base
   has_attached_file :logo, :styles => { :medium => "300x300#", :thumb => "100x100#" }
 
+
+  validates :name, :presence => true
+  validates :desc, :presence => true
+
   validates :logo, :attachment_presence => true
   validates_with AttachmentPresenceValidator, :attributes => :logo
   validates_with AttachmentSizeValidator, :attributes => :logo, :less_than => 1.megabytes
