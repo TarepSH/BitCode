1874485bafd22bdea22d2c307b8801bdd992b910
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-03-16 17:29:48 +0200

    Added is Admin and is User methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def user?
+    self.role == "user"
+  end
 end
