7735fa0a29ae4850c139877e78d8f33cc97a2fdf
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-03-16 17:33:28 +0200

    Added logo to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,19 @@
 class Course < ActiveRecord::Base
+  has_attached_file :logo, :styles => { :medium => "300x300#", :thumb => "100x100#" }
+
+  validates :logo, :attachment_presence => true
+  validates_with AttachmentPresenceValidator, :attributes => :logo
+  validates_with AttachmentSizeValidator, :attributes => :logo, :less_than => 1.megabytes
+
+  validates_attachment :logo, :presence => true,
+    :content_type => {
+      :content_type => [
+        "image/jpg",
+        "image/jpeg",
+        "image/png"
+      ]
+    },
+    :size => {
+      :less_than => 1.megabytes
+    }
 end
