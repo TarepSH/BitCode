b93835aa010d80ab2ba193e1868015934f40170f
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-03-16 17:52:01 +0200

    Added logo dimensions validator

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,4 +16,16 @@ class Course < ActiveRecord::Base
     :size => {
       :less_than => 1.megabytes
     }
+
+  validate :logo_dimensions
+
+  private
+
+  def logo_dimensions
+    required_width  = 600
+    required_height = 600
+    dimensions = Paperclip::Geometry.from_file(logo.queued_for_write[:original].path)
+
+    errors.add(:logo, "dimensions must be #{required_width}x#{required_height}") unless dimensions.width == required_width && dimensions.height == required_height
+  end
 end
