08c9d43170e3f96a18092a5d2f688b727ca86f7e
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-03-19 15:38:36 +0200

    Added I18n with "ar" as default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_filter :set_local
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  private
+
+  def set_local
+    I18n.locale = 'ar'
   end
 
 end
