4cbbea360e7125a9474b1a39e53f8a776ef72bf9
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-03-25 17:11:43 +0200

    Added new steps definition to match css and ids

--- app/matchers/html_css_matcher.rb
+++ app/matchers/html_css_matcher.rb
@@ -7,6 +7,69 @@ class HtmlCssMatcher
   end
 
   def run
+    /There is "(.*)" root hierarchy/.match(regex) do |res|
+      html_structure = Nokogiri::HTML(@code)
+      tags_array = res[1].split('>')
+
+      match_result = html_structure
+      tags_array.each do |tag|
+        tag_name, id, classes = tag_attribute(tag)
+        match_result = match_result.xpath(tag_name)
+
+        if (match_result.length > 0)
+          match_tag = match_result[0]
+          if (!id.nil?)
+            if (match_tag["id"] != id)
+              raise "#{tag_name} doesn't has id #{id}"
+            end
+          end
+
+          if (classes.length > 0)
+            if (!(classes - match_tag["class"].split(' ')).empty?)
+              raise "#{tag_name} doesn't has classes #{classes.join(' ')}"
+            end
+          end
+        else
+          raise "#{tag_name} not exsisted"
+        end
+      end
+      return true
+    end
+
+    /There is "(.*)" hierarchy/.match(regex) do |res|
+      first = true
+      html_structure = Nokogiri::HTML(@code)
+      tags_array = res[1].split('>')
+
+      match_result = html_structure
+      tags_array.each do |tag|
+        tag_name, id, classes = tag_attribute(tag)
+        if (first)
+          match_result = match_result.css(tag_name)
+        else
+          match_result = match_result.xpath(tag_name)
+        end
+
+        if (match_result.length > 0)
+          match_tag = match_result[0]
+          if (!id.nil?)
+            if (match_tag["id"] != id)
+              raise "#{tag_name} doesn't has id #{id}"
+            end
+          end
+
+          if (classes.length > 0)
+            if (!(classes - match_tag["class"].split(' ')).empty?)
+              raise "#{tag_name} doesn't has classes #{classes.join(' ')}"
+            end
+          end
+        else
+          raise "#{tag_name} not exsisted"
+        end
+      end
+      return true
+    end
+
     /"(.*)" tag is exsist/.match(regex) do |res|
       html_structure = Nokogiri::HTML(@code)
       match_result = html_structure.xpath(res[1])
@@ -48,5 +111,48 @@ class HtmlCssMatcher
 
     raise "No Matcher found."
   end
+
+  private
+
+    def build_path text
+      tags = []
+      path = ""
+      i = 1
+      tags_array = text.split('>')
+      tags_array.each do |tag_with_property|
+        tag = tag_with_property.split('.')[0]
+        tags << tag
+        path += tag
+        if (i < tags_array.length)
+          path += '//'
+        end
+        i += 1
+      end
+      [path, tags]
+    end
+
+    def tag_attribute tag
+      classes = []
+      id = ""
+
+      ch = tag.split('#')
+      classes_tmp = ch[0].split('.')
+      tag_name = classes_tmp[0]
+
+      classes_tmp = classes_tmp.slice(1, classes_tmp.length)
+      classes << classes_tmp
+
+      if (ch[1].nil?)
+        id = nil
+      else
+        classes_tmp = ch[1].split('.')
+        id = classes_tmp[0]
+
+        classes_tmp = classes_tmp.slice(1, classes_tmp.length)
+        classes << classes_tmp
+      end
+
+      [tag_name, id, classes.flatten!]
+    end
 
 end
--- steps-test.rb
+++ steps-test.rb
@@ -9,6 +9,72 @@ hcm = HtmlCssMatcher.new('"html" tag is exsist', '<html>
 hcm.run
 
 puts '-----'
+hcm = HtmlCssMatcher.new('There is "html>body>h1.head.hod#hind" root hierarchy', '<html>
+  <head>
+  </head>
+  <body>
+    <h1 class="head hod" id="hind">Hello, World</h1>
+    <img src="" alt="">
+  </body>
+  </html>')
+hcm.run
+
+puts '-----'
+hcm = HtmlCssMatcher.new('There is "html>body>h1.head.hod#hind" root hierarchy', '<html>
+  <head>
+  </head>
+  <body>
+    <h1 class="hod head" id="hind">Hello, World</h1>
+    <img src="" alt="">
+  </body>
+  </html>')
+hcm.run
+
+puts '-----'
+hcm = HtmlCssMatcher.new('There is "html>body>h1.head.hod#hind" root hierarchy', '<html>
+  <head>
+  </head>
+  <body>
+    <h1 class="head ha hod" id="hind">Hello, World</h1>
+    <img src="" alt="">
+  </body>
+  </html>')
+hcm.run
+
+puts '-----'
+hcm = HtmlCssMatcher.new('There is "html>body>h1.head.hod#hind" root hierarchy', '<html>
+  <head>
+  </head>
+  <body>
+    <h1 class="head" id="hind">Hello, World</h1>
+    <img src="" alt="">
+  </body>
+  </html>')
+hcm.run
+
+puts '-----'
+hcm = HtmlCssMatcher.new('There is "body>h1.head.hod#hind" hierarchy', '<html>
+  <head>
+  </head>
+  <body>
+    <h1 class="head hod" id="hind">Hello, World</h1>
+    <img src="" alt="">
+  </body>
+  </html>')
+hcm.run
+
+puts '-----'
+hcm = HtmlCssMatcher.new('There is "body>h1.head.hod#hind" hierarchy', '<html>
+  <head>
+  </head>
+  <body>
+    <h1 class="head hod" id="hend">Hello, World</h1>
+    <img src="" alt="">
+  </body>
+  </html>')
+hcm.run
+
+puts '-----'
 hcm = HtmlCssMatcher.new('There is "body" tag inside the "html" tag', '<html>
   <head>
   </head>
