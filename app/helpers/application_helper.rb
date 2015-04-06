72f4a519610d145c5d9c4787bad25cc1920d01c8
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-04-06 16:27:16 +0300

    Used gravatar image as profile photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
   def resource_name
    :user
- end
+  end
 
- def resource
+  def resource
    @resource ||= User.new
- end
+  end
 
- def devise_mapping
+  def devise_mapping
    @devise_mapping ||= Devise.mappings[:user]
- end
+  end
+
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
