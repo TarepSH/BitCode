dfe56e94dc0740d90b6a99bba06128c6d26ff889
Mohammed Aljuboori <mhdaljobory@gmail.com>
2015-04-20 16:42:40 +0300

    Added user badges to user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   def show
     @user = current_user
     @courses = @user.courses
+    @badges = @user.badges
     @other_courses = Course.where('id NOT IN (?)', @courses.map { |c| c.id })
   end
 
